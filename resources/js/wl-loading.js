1fbc0811ec84a7ca14e74cfafb7015e6439f4523
youkatei <yjt1218@gmail.com>
2021-03-20 04:53:30 +1100

    Updated code to current version of CitizenFx, and now tips are browser locale sensitive

--- resources/js/wl-loading.js
+++ resources/js/wl-loading.js
@@ -8,25 +8,29 @@ function ready(fn) {
 }
 
 ready(function () {
-    const tips = [
-        "[开发中]服务器正在开发中, 如果有任何玩法建议, 欢迎联系imckl@outlook.com",
-        "[开发中]赛车系统开发中, 敬请期待, 催进度请联系Her0mAn",
-        // "[提醒]你可以使用'/car 车名'来出生载具, 例如'/car infernus'",
-        // "[提醒]'/tp qms'用于传送至秋名山底, '/tp qmsd'则是山顶",
-        // "[提醒]'/tpmake'可用于创建属于你的传送点",
-        // "[测试中]地图上的骷髅标点可以做警匪PvE任务",
-        // "[测试中]警车上按住E, 可以触发类似GTA IV的警察任务"
-        "[提醒]按M键使用玩家菜单"
+    // "|" is the separator for tips legend and tips content
+    const tipsEN = [
+        "Develop|The server is undergoing development, if you have any suggestions, feel free to contact imckl@outlook.com",
+        "Develop|Racing system is undergoing development, please look forward to it. @Her0mAn if you want it done faster :)",
+        "Tips|Press M to use player menu"
+    ];
+
+    const tipsCN = [
+        "开发|服务器正在开发中, 如果有任何玩法建议, 欢迎联系imckl@outlook.com",
+        "开发|赛车系统开发中, 敬请期待, 催进度请联系Her0mAn",
+        "提醒|按M键使用玩家菜单"
     ];
 
     const handlers = {
         // final stage of game loading, fade out music
         loadProgress(data) {
-            if (data.loadFraction * 100 >= 90) {
+            if (data.loadFraction >= 0.90) {
                 setTimeout(fade, 2000);
             }
         }
     };
+
+    const locale = navigator.language.toLocaleLowerCase();
 
     let iconEl = document.getElementById("spinner");
     let tipsLegendEl = document.getElementById("tips-legend");
@@ -40,45 +44,48 @@ ready(function () {
     let src = document.createElement('source');
     src.src = 'music/gtav-outro.ogg';
     src.type = 'audio/ogg';
-    soundFile.appendChild(src);
 
     //Plays the sound
-    let playSound = function () {
-        //Set the current time for the audio file to the beginning
+    let playMusic = function () {
+        soundFile.appendChild(src);
+        soundFile.load();
         soundFile.currentTime = 0.01;
         soundFile.volume = 0.3;
         //Due to a bug in Firefox, the audio needs to be played after a delay
-        setTimeout(function () { soundFile.play(); }, 1);
+        setTimeout(function () {
+            soundFile.play();
+        }, 1);
     }
 
-    // make icon and legend appear more naturally
+    // make loading icon appear more naturally
     let naturalAppear = function () {
         iconEl.classList.add("hide");
-        tipsLegendEl.classList.add("hide");
-        setTimeout(function () {
-            tipsLegendEl.innerText = "Tips";
-        }, 1000);
         setTimeout(function () {
             iconEl.classList.remove("hide");
-            tipsLegendEl.classList.remove("hide");
         }, 1000);
     };
 
     // loop through tips
-    let tipsLoop = function () {
-        let index = Math.floor(Math.random() * tips.length);
+    let tipsLoop = function (tipsList) {
+        let index = Math.floor(Math.random() * tipsList.length);
         while (index === previousIndex) {
-            index = Math.floor(Math.random() * tips.length);
+            index = Math.floor(Math.random() * tipsList.length);
         }
         previousIndex = index;
+        let splitTipEntry = tipsList[index].split("|");
+        tipsLegendEl.classList.add("hide");
         tipsContentEl.classList.add("hide");
         setTimeout(function () {
-            tipsContentEl.innerText = tips[index];
+            tipsLegendEl.innerText = splitTipEntry[0];
+            tipsContentEl.innerText = splitTipEntry[1];
         }, 1000);
         setTimeout(function () {
+            tipsLegendEl.classList.remove("hide");
             tipsContentEl.classList.remove("hide");
         }, 1000);
-        setTimeout(tipsLoop, 7000);
+        setTimeout(function () {
+            tipsLoop(tipsList)
+        }, 7000);
     };
 
     // for music to fade out
@@ -89,12 +96,17 @@ ready(function () {
         }
     }
 
-    soundFile.load();
-    playSound();
+    playMusic();
     naturalAppear();
-    tipsLoop();
+    if (locale.startsWith("zh")) {
+        tipsLoop(tipsCN);
+    } else {
+        tipsLoop(tipsEN);
+    }
+
 
     window.addEventListener('message', function (e) {
-        (handlers[e.data.eventName] || function () { })(e.data);
+        (handlers[e.data.eventName] || function () {
+        })(e.data);
     });
 });
